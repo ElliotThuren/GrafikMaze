bf70e03c890159014af01d369d1e2f7c16e9e129
ElliotThuren <elliot.thuren@elev.ga.ntig.se>
2021-03-23 16:18:01 +0100

    Har gjort klart labyrinten som bild, men inga animationer finns ännu

--- src/sample/GrafikMaze.java
+++ src/sample/GrafikMaze.java
@@ -12,8 +12,8 @@ public class GrafikMaze extends Canvas {
     private BufferStrategy bs;
 
     public GrafikMaze() {
-        JFrame frame = new JFrame("An empty canvas");
-        this.setSize(800, 600);
+        JFrame frame = new JFrame("Killer Maze Raze!");
+        this.setSize(606, 606);
         frame.add(this);
         frame.pack();
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -44,10 +44,32 @@ public class GrafikMaze extends Canvas {
         drawWallX(g, 150,150);
         drawWallX(g, 150,250);
         drawWallX(g, 250,350);
+        drawWallX(g, 350,450);
+        drawWallY(g, 350,250);
+        drawWallY(g, 350,150);
+        drawWallY(g, 350,450);
+        drawWallY(g, 450,350);
+        drawWallY(g, 450,250);
+        drawWallY(g, 450,150);
         drawWallX(g, 50,350);
         drawWallY(g, 150,450);
         drawWallY(g, 250,250);
         drawWallY(g, 250,350);
+        drawfixdot(g, 250,150);
+        drawfixdot(g, 150,350);
+        drawfixdot(g, 250,450);
+        drawfixdot(g, 350,350);
+        drawfixdot(g, 450,450);
+        drawfixdot(g, 550,550);
+        drawenemydot(g, 96,296);
+        drawenemydot(g, 96,496);
+        drawenemydot(g, 496,196);
+        drawenemydot(g, 196,196);
+        drawenemydot(g, 296,296);
+        drawenemydot(g, 296,396);
+        drawenemydot(g, 496,496);
+        drawplayerdot(g, 90,90);
+        drawGoal(g, 356,456);
     }
 
     private void drawWallY(Graphics g, int x, int y) {
@@ -68,6 +90,26 @@ public class GrafikMaze extends Canvas {
     private void drawoutlineWallX(Graphics g, int x, int y) {
         g.setColor(new Color(0, 0, 0));
         g.fillRect(x, y, 500, 6);
+    }
+
+    private void drawfixdot(Graphics g, int x, int y) {
+        g.setColor(new Color(0, 0, 0));
+        g.fillRect(x, y, 6, 6);
+    }
+
+    private void drawenemydot(Graphics g, int x, int y) {
+        g.setColor(new Color(255, 0, 0));
+        g.fillRect(x, y, 12, 12);
+    }
+
+    private void drawplayerdot(Graphics g, int x, int y) {
+        g.setColor(new Color(20, 37, 180));
+        g.fillRect(x, y, 24, 24);
+    }
+
+    private void drawGoal(Graphics g, int x, int y) {
+        g.setColor(new Color(25, 239, 6));
+        g.fillRect(x, y, 15, 94);
     }
 
     public static void main(String[] args) {
