bebff0d358795eb938ee4e931e71c35e3a18a38a
ElliotThuren <elliot.thuren@elev.ga.ntig.se>
2021-03-25 11:23:15 +0100

    Har sorterat koden lite bättre

--- src/sample/GrafikMaze.java
+++ src/sample/GrafikMaze.java
@@ -6,8 +6,8 @@ import java.awt.image.BufferStrategy;
 
 
 public class GrafikMaze extends Canvas {
-    private int width = 800;
-    private int height = 600;
+    private int width = 606;
+    private int height = 606;
 
     private BufferStrategy bs;
 
@@ -35,40 +35,40 @@ public class GrafikMaze extends Canvas {
     }
 
     public void paint(Graphics g) {
-        drawoutlineWallY(g, 50,50);
-        drawoutlineWallX(g, 50,50);
-        drawoutlineWallY(g, 550,50);
-        drawoutlineWallX(g, 50,550);
-        drawWallY(g, 150,50);
-        drawWallY(g, 150,150);
+        drawOutlineWallX(g, 50,50);
+        drawOutlineWallX(g, 50,550);
+        drawOutlineWallY(g, 50,50);
+        drawOutlineWallY(g, 550,50);
         drawWallX(g, 150,150);
         drawWallX(g, 150,250);
         drawWallX(g, 250,350);
         drawWallX(g, 350,450);
+        drawWallX(g, 50,350);
+        drawWallY(g, 150,50);
+        drawWallY(g, 150,150);
         drawWallY(g, 350,250);
         drawWallY(g, 350,150);
         drawWallY(g, 350,450);
         drawWallY(g, 450,350);
         drawWallY(g, 450,250);
         drawWallY(g, 450,150);
-        drawWallX(g, 50,350);
         drawWallY(g, 150,450);
         drawWallY(g, 250,250);
         drawWallY(g, 250,350);
-        drawfixdot(g, 250,150);
-        drawfixdot(g, 150,350);
-        drawfixdot(g, 250,450);
-        drawfixdot(g, 350,350);
-        drawfixdot(g, 450,450);
-        drawfixdot(g, 550,550);
-        drawenemydot(g, 96,296);
-        drawenemydot(g, 96,496);
-        drawenemydot(g, 496,196);
-        drawenemydot(g, 196,196);
-        drawenemydot(g, 296,296);
-        drawenemydot(g, 296,396);
-        drawenemydot(g, 496,496);
-        drawplayerdot(g, 90,90);
+        drawFixdot(g, 250,150);
+        drawFixdot(g, 150,350);
+        drawFixdot(g, 250,450);
+        drawFixdot(g, 350,350);
+        drawFixdot(g, 450,450);
+        drawFixdot(g, 550,550);
+        drawEnemydot(g, 96,296);
+        drawEnemydot(g, 96,496);
+        drawEnemydot(g, 496,196);
+        drawEnemydot(g, 196,196);
+        drawEnemydot(g, 296,296);
+        drawEnemydot(g, 296,396);
+        drawEnemydot(g, 496,496);
+        drawPlayerdot(g, 90,90);
         drawGoal(g, 356,456);
     }
 
@@ -82,27 +82,27 @@ public class GrafikMaze extends Canvas {
         g.fillRect(x, y, 100, 6);
     }
 
-    private void drawoutlineWallY(Graphics g, int x, int y) {
+    private void drawOutlineWallY(Graphics g, int x, int y) {
         g.setColor(new Color(0, 0, 0));
         g.fillRect(x, y, 6, 500);
     }
 
-    private void drawoutlineWallX(Graphics g, int x, int y) {
+    private void drawOutlineWallX(Graphics g, int x, int y) {
         g.setColor(new Color(0, 0, 0));
         g.fillRect(x, y, 500, 6);
     }
 
-    private void drawfixdot(Graphics g, int x, int y) {
+    private void drawFixdot(Graphics g, int x, int y) {
         g.setColor(new Color(0, 0, 0));
         g.fillRect(x, y, 6, 6);
     }
 
-    private void drawenemydot(Graphics g, int x, int y) {
+    private void drawEnemydot(Graphics g, int x, int y) {
         g.setColor(new Color(255, 0, 0));
         g.fillRect(x, y, 12, 12);
     }
 
-    private void drawplayerdot(Graphics g, int x, int y) {
+    private void drawPlayerdot(Graphics g, int x, int y) {
         g.setColor(new Color(20, 37, 180));
         g.fillRect(x, y, 24, 24);
     }
